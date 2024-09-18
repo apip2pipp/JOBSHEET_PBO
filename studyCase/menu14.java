f4e044729050750ec33be702f7a61a3a8353ca24
Muhammad Afif Khosyidzaki <afifkhosyidzaki@gmail.com>
2024-09-18 08:02:52 +0700

    task commit

---
+++ studyCase/menu14.java
@@ -0,0 +1,35 @@
+package studyCase;
+
+import java.util.ArrayList;
+
+public class menu14 {
+    private String namaMakanan;
+    private double harga;
+    private String kategori;
+    private ArrayList<pesanan14> daftarPesanan;
+
+    public menu14(String namaMakanan, double harga, String kategori) {
+        this.namaMakanan = namaMakanan;
+        this.setHarga(harga);
+        this.kategori = kategori;
+        this.daftarPesanan = new ArrayList<>();
+    }
+
+    public double getHarga() {
+        return harga;
+        
+    }
+
+    public void setHarga(double harga) {
+        this.harga = harga;
+        
+    }
+
+    public void tambahPesanan(pesanan14 pesanan) {
+        daftarPesanan.add(pesanan);
+    }
+
+    public String getInfo() {
+        return "Menu: " + namaMakanan + ", Harga: " + getHarga() + ", Kategori: " + kategori;
+    }
+}
