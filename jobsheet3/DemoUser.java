8c050329785d9106ecd433ee9b554652aff93338
Muhammad Afif Khosyidzaki <afifkhosyidzaki@gmail.com>
2024-09-30 08:45:40 +0700

    rename delete

--- jobsheet3/DemoUser.java
+++
@@ -1,8 +0,0 @@
-package jobsheet3;
-public class DemoUser {
-    public static void main(String[] args) {
-        Users user1 = new Users("annisa.nadya", "Annisa Nadya", "annisa.nadya@gmail.com");
-
-        user1.cetakInfo();
-    }
-}
