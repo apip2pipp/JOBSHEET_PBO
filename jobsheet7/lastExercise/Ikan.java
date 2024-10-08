cd3d8bf73b022c4594458aacbc088c4dc1168160
Muhammad Afif Khosyidzaki <afifkhosyidzaki@gmail.com>
2024-10-09 04:41:50 +0700

    JOBSHEET 7 PRAK PBO - PROJECT

---
+++ jobsheet7/lastExercise/Ikan.java
@@ -0,0 +1,8 @@
+package jobsheet7.lastExercise;
+
+public class Ikan {
+    public void swim() {
+        System.out.println("Ikan bisa berenang");
+    }
+
+}
---
+++ jobsheet7/lastExercise/Piranha.java
@@ -0,0 +1,9 @@
+package jobsheet7.lastExercise;
+
+public class Piranha extends Ikan{
+    @Override
+    public void swim() {
+        System.out.println("Piranha bisa makan daging");
+    }
+
+}
